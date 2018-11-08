7ccc85767eb8af5edcf0cb3a5afcc50518b6e297
Viktor Starynskyi <vstarynsky@gmail.com>
2018-11-08 11:50:21 +0200

    Module #5

---
+++ client/store.js
@@ -0,0 +1,19 @@
+import { createStore, sompose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+// import root reducer
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+// create an object for the default state
+const defaultState = {
+	posts,
+	comments
+};
+
+export default const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
