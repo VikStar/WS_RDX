732e6f4131c79f7f200bcc91c2e1079bc21a5d48
Viktor Starynskyi <vstarynsky@gmail.com>
2018-11-08 11:37:16 +0200

    Module #4

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -2,8 +2,21 @@ import React from 'react';
 import { render } from 'react-dom';
 import css from './styles/style.styl';
 import Main from './components/Main';
+import PhotoGrid from './components/PhotoGrid';
+import Single from './components/Single';
+
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+	<Router history={browserHistory}>
+		<Route path="/" component={Main}>
+			<IndexRoute component={PhotoGrid}></IndexRoute>
+			<Route path="/view/:postId" component={Single}></Route>
+		</Route>
+	</Router>
+);
 
 render(
-	<Main />,
+	router,
 	document.getElementById('root')
 );
