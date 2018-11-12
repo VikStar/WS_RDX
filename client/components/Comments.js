d5a64c30c90f6a0e608ff78cdef596d599bea225
Viktor Starynskyi <vstarynsky@gmail.com>
2018-11-12 18:55:51 +0200

    Module #15

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React from "react";
 
 class Comments extends React.Component {
+	constructor() {
+		super();
+
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
 	renderComment(comment, i) {
 		return (
 			<div className="comment" key={i}>
@@ -13,13 +19,22 @@ class Comments extends React.Component {
 		);
 	}
 
+	handleSubmit(e) {
+		e.preventDefault();
+		const { postId } = this.props.params;
+		const author = this.refs.author.value;
+		const comment = this.refs.comment.value;
+		this.props.addComment(postId, author, comment);
+	}
+
 	render() {
 		return (
 			<div className="comments">
 				{this.props.postComments.map(this.renderComment)}
-				<form ref='commentForm' className="comment-form">
-					<input type="text" ref='author' placeholder='author' />
-					<input type="text" ref='comment' placeholder='comment' />
+
+				<form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
+					<input type="text" ref="author" placeholder="author" />
+					<input type="text" ref="comment" placeholder="comment" />
 					<input type="submit" hidden />
 				</form>
 			</div>
--- client/components/Single.js
+++ client/components/Single.js
@@ -12,7 +12,7 @@ export default class Single extends React.Component {
 		return (
 			<div className="single-photo">
 				<Photo i={i} post={post} {...this.props} />
-				<Comments postComments={postComments} />
+				<Comments postComments={postComments} {...this.props} />
 			</div>
 		);
 	}
