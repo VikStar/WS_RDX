0815eb7459ae357700d4a09012f6f01d79f75bd1
Viktor Starynskyi <vstarynsky@gmail.com>
2018-11-12 19:24:50 +0200

    Module #16

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -4,16 +4,18 @@ class Comments extends React.Component {
 	constructor() {
 		super();
 
+		this.renderComment = this.renderComment.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
 	renderComment(comment, i) {
+		const { postId } = this.props.params;
 		return (
 			<div className="comment" key={i}>
 				<p>
 					<strong>{comment.user}</strong>
 					{comment.text}
-					<button className="remove-comment">&times;</button>
+					<button className="remove-comment" onClick={() => this.props.removeComment(postId, i)}>&times;</button>
 				</p>
 			</div>
 		);
@@ -25,6 +27,7 @@ class Comments extends React.Component {
 		const author = this.refs.author.value;
 		const comment = this.refs.comment.value;
 		this.props.addComment(postId, author, comment);
+		this.refs.commentForm.reset();
 	}
 
 	render() {
--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,4 +1,31 @@
-export default function comments(state = [], action) {
-	// console.log(state, action);
+function comments(state = [], action) {
+	if (typeof action.postId !== 'undefined') {
+		return {
+			// take the current state
+			...state,
+			// overwrite this post with a new one
+			[action.postId]: postComments(state[action.postId], action)
+		};
+	}
+	
 	return state;
 }
+
+function postComments(state = [], action) {
+	switch (action.type) {
+		case 'ADD_COMMENT':
+			return [...state, {
+				user: action.author,
+				text: action.comment
+			}];
+		case 'REMOVE_COMMENT':
+			return [
+				...state.slice(0, action.i),
+				...state.slice(action.i + 1)
+			];
+		default:
+			return state;
+	}
+}
+
+export default comments;
