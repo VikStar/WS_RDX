b0461c7decef2a6865e45116d4b4599bb47dc852
Viktor Starynskyi <vstarynsky@gmail.com>
2018-11-12 18:18:53 +0200

    Module #13

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 export default class Single extends React.Component {
 	render() {
+		const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+		const post = this.props.posts[i];
+
 		return (
 			<div className="single-photo">
-				I'm the single
+				<Photo i={i} post={post} {...this.props} />
+				<Comments />
 			</div>
 		);
 	}
